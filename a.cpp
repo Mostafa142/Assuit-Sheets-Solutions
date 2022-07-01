3bfd01e1a9616e99bbb97ac3e95c3eeecb8e4ed1
Mostafa142 <mostafafathi705@gmail.com>
2022-07-01 15:59:52 +0200

    add solutions

---
+++ a.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	string fullname;
+	cin>>fullname;
+	cout<<"Hello, "<<fullname;
+}
---
+++ b.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+	int n1;
+	long long n2;
+	char text;
+	float n3;
+	double n4;
+	cin>>n1>>n2>>text>>n3>>n4;
+	cout<<n1<<endl;
+	cout<<n2<<endl;
+	cout<<text<<endl;
+	cout<<n3<<endl;
+	cout<<n4<<endl;
+}
