a54263cdeaaba1e2e49a08ced2f7463657c72f4a
Mostafa142 <mostafafathi705@gmail.com>
2022-07-03 15:34:15 +0200

    D solution

---
+++ e.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+double radius;
+const double pi=3.141592653;
+cin>>radius;
+     cout << fixed << setprecision(9);
+	cout<<pi*radius*radius;
+	
+}
