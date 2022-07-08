780d84c527db4b25c545bb69256f75dd5aa952a6
Mostafa142 <mostafafathi705@gmail.com>
2022-07-08 18:32:14 +0200

    M & N Solution

---
+++ m.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+char x;
+cin>>x;
+if(x>=97&&x<=122){
+	cout<<"ALPHA"<<endl;
+	cout<<"IS SMALL"<<endl;
+}
+else if (x>=65&&x<=90){
+	cout<<"ALPHA"<<endl;
+	cout<<"IS CAPITAL"<<endl;
+}
+else{
+		cout<<"IS DIGIT"<<endl;
+ 
+}
+}
---
+++ n.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+char x,y;
+cin>>x;
+if(x>=97){
+	y=x-32;
+	cout<<y;
+}
+else{
+	y=x+32;
+	cout<<y;
+}
+}
