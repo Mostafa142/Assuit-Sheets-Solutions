273d70cef1e7694feeba9835d95c1e31baf411de
Mostafa142 <mostafafathi705@gmail.com>
2022-08-04 19:33:21 +0200

    Y-Easy Fibonacci Solution

--- Sheet #2/Y-Easy Fibonacci.cpp
+++ Sheet #2/Y-Easy Fibonacci.cpp
@@ -1,15 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  int n1=0,n2=1,n3,i,number;    
- cin>>number;    
- cout<<n1<<" "<<n2<<" "; //printing 0 and 1    
- for(i=2;i<number;++i) //loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  cout<<n3<<" ";    
-  n1=n2;    
-  n2=n3;    
- }    
-   return 0;  
+   int size;
+   cin>>size;
+   int start = 0 , second =1;
+   if(size == 1){
+   	cout<<start<<endl;
+   }else if( size == 2){
+   	cout<<start<<" "<<second<<endl;
+   }else{
+   	cout<<start<<" "<<second<<" ";;
+   	for(int i=2 ; i<size ; i++){
+   		int res = start +second;
+   		cout<<res<<" ";
+   		start = second;
+   		second = res;
+	   }
+   }
 }
