39bd791094513b8eccf6926b7bbfc6e1fbe651d6
Mostafa142 <mostafafathi705@gmail.com>
2022-08-07 21:52:27 +0200

    Still Wrong

--- Sheet #2/W-Shape3.cpp
+++ Sheet #2/W-Shape3.cpp
@@ -1,26 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int rows,i,j,k;
-    cin>>rows;
-    for(i=1; i<=rows; i++){   
-    for(j=1; j<=rows-i; j++){
-      cout<<" ";
+	int i,j,r;
+    cin >> r;
+   for(i=1;i<=r;i++)
+   {
+     for(j=1;j<=r-i;j++)
+     cout<<" ";
+     for(j=1;j<=2*i-1;j++)
+       cout<<"*";
+     cout<<endl;
+   }
+   for(i=r-1;i>=1;i--)
+   {
+     for(j=1;j<=r-i;j++)
+     cout<<" ";
+     for(j=1;j<=2*i-1;j++)
+       cout<<"*";
+     cout<<endl;
 }
-for(k=1; k<=i; k++){
-      cout<<"* ";
-     cout<<"";
-}
-cout<<"\n";
-}
-for(i=1; i<=rows-1; i++){ 
-    for(j=1; j<=i; j++){
-      cout<<" ";
-}
-for(k=1; k<=rows-i; k++){
-      cout<<"* ";
-      cout<<"";
-}
-cout<<"\n";
-        }
 }
