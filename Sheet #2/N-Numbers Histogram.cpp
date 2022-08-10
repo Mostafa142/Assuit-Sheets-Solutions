24a72af281111a4e5b3d1e9186bb88184fcb0e6a
Mostafa142 <mostafafathi705@gmail.com>
2022-08-10 14:54:08 +0200

    N-Numbers Histogram Solution

---
+++ Sheet #2/N-Numbers Histogram.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+   int n, x;
+	char ch;
+	cin >> ch >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >>x;
+		for (int i = 0; i < x; i++)
+		{
+			cout << ch;
+		}
+		cout << "\n";
+	}
+	return 0;
+}
