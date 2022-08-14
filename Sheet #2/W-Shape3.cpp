60501535dd4eca81501f7a045964e4b9ae24c901
Mostafa142 <mostafafathi705@gmail.com>
2022-08-14 21:31:01 +0200

    Fault

--- Sheet #2/W-Shape3.cpp
+++ Sheet #2/W-Shape3.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-	int i,j,row;
+	int i,k,row;
     cin >> row;
    for(i=1;i<=row;i++)
    {
-     for(j=1;j<=row-i;j++)
+     for(k=1;k<=row-i;k++)
      cout<<" ";
-     for(j=1;j<=2*i-1;j++)
+     for(k=1;k<=2*i-1;k++)
        cout<<"*";
      cout<<endl;
    }
    for(i=row-1;i>=1;i--)
    {
-     for(j=1;j<=row-i;j++)
+     for(k=1;k<=row-i;k++)
      cout<<" ";
-     for(j=1;j<=2*i-1;j++)
+     for(k=1;k<=2*i-1;k++)
        cout<<"*";
      cout<<endl;
 }
